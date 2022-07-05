33edf27c4dd43683835c01ee24f7f2897fb77936
jna2158 <jna215843@gmail.com>
2022-07-04 23:53:06 -0700

    feat: Today 컴포넌트에서 오늘의 날짜 작성 중

--- src/Today.js
+++ src/Today.js
@@ -1,13 +1,29 @@
 import React from "react";
 
-class Today extends React.Component {
-  render() {
-    return (
-      <>
-        <div>Today</div>
-      </>
-    );
-  }
+// class Today extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <div>{this.getDate()}</div>
+//       </>
+//     );
+//   }
+//   getDate() {
+//     let today = new Date();
+//     console.log(today);
+//   }
+// }
+function Today() {
+  const getDate = () => {
+    var today = new Date();
+
+    var year = today.getFullYear();
+    var month = ("0" + (today.getMonth() + 1)).slice(-2);
+    var day = ("0" + today.getDate()).slice(-2);
+    var dateString = year + "-" + month + "-" + day;
+    return dateString;
+  };
+  return <div>{getDate()}</div>;
 }
 
 export default Today;
